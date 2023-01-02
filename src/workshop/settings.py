906d12c5be7ce7082cda53b8f01968f661c2a85f
second <maxchizh24@mail.ru>
2023-01-02 23:17:29 +0300

    second commit

--- src/workshop/settings.py
+++ src/workshop/settings.py
@@ -1,10 +1,11 @@
 from pydantic import BaseSettings
+import psycopg2
 
 
 class Settings(BaseSettings):
     server_host: str = "127.0.0.1"
     sever_port: int = 8000
-    database_url: str = 'postgresql+psycopg2://user:password@hostname/database'
+    database_url: str = 'postgresql+psycopg2://postgres:root@localhost/workshop'
 
 
 settings = Settings()
--- src/workshop/tables.py
+++ src/workshop/tables.py
@@ -7,10 +7,10 @@ Base = declarative_base()
 
 class Operation(Base):
     __tablename__ = "operation"
-
     id = sa.Column(sa.Integer, primary_key=True)
     date = sa.Column(sa.Date)
     kind = sa.Column(sa.String)
     amount = sa.Column(sa.Numeric(10, 2))
     description = sa.Column(sa.String, nullable=True)
+
 
